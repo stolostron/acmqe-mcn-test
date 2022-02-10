84af4bf42765fd8133d61eb3bf250494ee618b9b
Maxim Babushkin <mbabushk@redhat.com>
2022-02-10 17:23:24 +0200

    Support multiple clusters test
    Add the ability to test multiple managed clusters with submariner
    component installed.
    The test flow will test all available submariner clusters and in case
    tests failures were present, will print them at the end.

--- lib/submariner_test.sh
+++ lib/submariner_test.sh
@@ -30,23 +30,62 @@ function execute_submariner_tests() {
 
         INFO "Show all Submariner information"
         subctl show all 2>&1 \
-            | tee  "$TESTS_LOGS/subctl_show_all_${primary_test_cluster}_${secondary_test_cluster}.log"
+            | tee  "$TESTS_LOGS/subctl_show_all_${primary_test_cluster}_${secondary_test_cluster}.log" \
+            || add_test_error $?
 
         INFO "Execute diagnose all tests"
         subctl diagnose all 2>&1 \
-            | tee "$TESTS_LOGS/subctl_diagnose_all_${primary_test_cluster}_${secondary_test_cluster}.log"
+            | tee "$TESTS_LOGS/subctl_diagnose_all_${primary_test_cluster}_${secondary_test_cluster}.log" \
+            || add_test_error $?
 
         INFO "Execute diagnose firewall inter-cluster tests"
         subctl diagnose firewall inter-cluster \
             "$TESTS_LOGS/$primary_test_cluster-kubeconfig.yaml" \
             "$TESTS_LOGS/$secondary_test_cluster-kubeconfig.yaml" 2>&1 \
-            |  tee "$TESTS_LOGS/subctl_firewall_tests_${primary_test_cluster}_${secondary_test_cluster}.log"
+            |  tee "$TESTS_LOGS/subctl_firewall_tests_${primary_test_cluster}_${secondary_test_cluster}.log" \
+            || add_test_error $?
 
         INFO "Execute E2E tests"
         subctl verify --only service-discovery,connectivity --verbose \
             --kubecontexts "$primary_test_cluster","$secondary_test_cluster" 2>&1 \
-            | tee "$TESTS_LOGS/subctl_e2e_tests_${primary_test_cluster}_${secondary_test_cluster}.log"
+            | tee "$TESTS_LOGS/subctl_e2e_tests_${primary_test_cluster}_${secondary_test_cluster}.log" \
+            || add_test_error $?
     done
-    INFO "All the tests finished successfully"
+    INFO "Tests execution finished"
     INFO "All the logs are placed within the $TESTS_LOGS directory"
+}
+
+# When one of the tests fails, add the error note to a global variable.
+# At the end of all tests, the variable will be checked.
+# If error occured, tests errors will be printed
+function add_test_error() {
+    local test_exit_code="$1"
+
+    if (( "$test_exit_code" > 0 )); then
+        WARNING "One of the tests failed... Adding to log"
+        export TESTS_FAILURES="true"
+    fi
+}
+
+function get_tests_failures() {
+    WARNING "The following tests failures occured"
+
+    local diagnose_logs
+    local e2e_tests_log
+
+    for log in "$TESTS_LOGS"/*.log; do
+        diagnose_logs=$(grep --no-messages '✗' "$log" || true)
+        if [[ -n "$diagnose_logs" ]]; then
+            echo
+            WARNING "Erros found in the following tests - $log"
+            echo "$diagnose_logs"
+        fi
+
+        e2e_tests_log=$(sed -n -e '/Summarizing.*.Failures/,$p' "$log")
+        if [[ -n "$e2e_tests_log" ]]; then
+            echo
+            WARNING "Erros found in the following tests - $log"
+            echo "$e2e_tests_log"
+        fi
+    done
 }
