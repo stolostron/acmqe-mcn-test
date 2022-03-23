13be00080be94e72763cbb6123f1609b3932bdc6
Maxim Babushkin <mbabushk@redhat.com>
2022-03-23 16:35:47 +0200

    Fix timeout definitions (#38)
    Set the timeout definitions to reset for every loop iterations.

--- lib/submariner_deploy.sh
+++ lib/submariner_deploy.sh
@@ -107,10 +107,10 @@ function wait_for_submariner_ready_state() {
     local cmd_output
 
     INFO "Check Submariner Gateway node state on clusters"
-    timeout=0
     cmd_output=""
     for cluster in $MANAGED_CLUSTERS; do
         INFO "Checking Submariner Gateway node state on $cluster cluster"
+        timeout=0
         until [[ "$timeout" -eq "$wait_timeout" ]] || [[ "$cmd_output" == "SubmarinerGatewayNodesLabeled" ]]; do
             INFO "Deploying..."
             cmd_output=$(check_submariner_deployment_state "$cluster" "SubmarinerGatewayNodesLabeled")
@@ -125,10 +125,10 @@ function wait_for_submariner_ready_state() {
     INFO "Submariner Gateway node has been sucesfully deployed on each cluster"
 
     INFO "Check Submariner Agent state on clusters"
-    timeout=0
     cmd_output=""
     for cluster in $MANAGED_CLUSTERS; do
         INFO "Checking Submariner Agent state on $cluster cluster"
+        timeout=0
         until [[ "$timeout" -eq "$wait_timeout" ]] || [[ "$cmd_output" == "SubmarinerAgentDeployed" ]]; do
             INFO "Deploying..."
             cmd_output=$(check_submariner_deployment_state "$cluster" "SubmarinerAgentDegraded")
@@ -143,10 +143,10 @@ function wait_for_submariner_ready_state() {
     INFO "Submariner Agent has been sucesfully deployed on each cluster"
 
     INFO "Check Submariner connectivity between clusters"
-    timeout=0
     cmd_output=""
     for cluster in $MANAGED_CLUSTERS; do
         INFO "Checking Submariner connectivity on $cluster cluster"
+        timeout=0
         until [[ "$timeout" -eq "$wait_timeout" ]] || [[ "$cmd_output" == "ConnectionsEstablished" ]]; do
             INFO "Deploying..."
             cmd_output=$(check_submariner_deployment_state "$cluster" "SubmarinerConnectionDegraded")
