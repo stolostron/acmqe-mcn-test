a5a61eecd37302b728967dc7786032d692d32a1d
Noga Tal <ntal@ntal.remote.csb>
2022-12-27 13:20:54 +0200

    Create uninstall test and validation test fix
    - Add new test for submariner uninstall flow
    - Modify element count fix for deployment validation test
    - Add "Skip" to the uninstall test in case the cluster set doesn't exist.
    - Remove unnecessary comments
    - Adding the missing /cypress/cypress/apis/clusterSet file
    - Remove "$list" variable from deployment validation test
    - Iincrease the timeout on uninstall test

---
+++ cypress/cypress/apis/clusterSet.js
@@ -0,0 +1,35 @@
+/** *****************************************************************************
+ * Licensed Materials - Property of Red Hat, Inc.
+ * Copyright (c) 2022 Red Hat, Inc.
+ ****************************************************************************** */
+/// <reference types="cypress" />
+
+import * as constants from "../support/constants"
+
+var headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+}
+
+/**
+* Get the clusterset info
+* @param {string} clusterSet
+* @returns {resp}
+*/
+export const getClusterSet = (clusterSet) => {
+    headers.Authorization = `Bearer ${Cypress.env("token")}`
+    let options = {
+        method: "GET",
+        url:
+            constants.apiUrl +
+            constants.ocm_cluster_api_v1beta2_path +
+            constants.managedclustersets_path +
+            '/' + clusterSet,
+        headers: headers,
+        failOnStatusCode: false
+    }
+    return cy.request(options)
+        .then(resp => {
+            return resp
+        })
+}
