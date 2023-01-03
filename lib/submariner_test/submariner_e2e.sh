068557ffdc0a057a44ad216351ff4ccf18e12eb2
Maxim Babushkin <mbabushk@redhat.com>
2023-01-03 19:56:33 +0200

    Integrate Cypress into the test pipeline (#153)
    - Add cypress tests execution into the pipeline
    
    - Split e2e and ui testing into different files.
      Both are called from the same "execute_submariner_tests" function.
    
    - Cypress requires "npm" and "nodejs" installed as prerequisites.
      Since those packages requires admin privileges and can't be installed
      during ci pipeline execution, the pipeline will check the state of the
      environment for those packages.
      If package will be missing, cypress execution will be skipped.
    
    - Add a new flag for testing execution - "--test-type".
      The flag controls execution of tests - e2e (api) and ui (cypress).
      By default, both testing methods will be mentioned, but a user count
      change it if needed.
    
    - Refactor "combine_tests_basename" function to generate tests basenames
      for both e2e and ui testing.

---
+++ lib/submariner_test/submariner_e2e.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# The function executes Submariner E2E tests by using the subctl tool.
+# The subctl tool is able to run E2E tests only on two clusters
+# at the same time.
+# In order to be able to test all clusters which contains
+# submariner addon, the function below is using the first managed
+# cluster as a primary cluster and all other clusters will run E2E
+# tests with the primary cluster.
+function execute_submariner_e2e_tests() {
+    INFO "Execute Submariner E2E tests"
+
+    # Subctl E2E tests are working with 2 clusters only at a time
+    local primary_test_cluster
+    local secondary_test_cluster
+    local tests_basename
+
+    primary_test_cluster=$(echo "$MANAGED_CLUSTERS" | head -n 1)
+
+    for cluster in $MANAGED_CLUSTERS; do
+        if [[ "$cluster" == "$primary_test_cluster" ]]; then
+            continue
+        fi
+
+        secondary_test_cluster="$cluster"
+        tests_basename=$(combine_tests_basename "e2e" "$primary_test_cluster" "$secondary_test_cluster")
+
+        INFO "Running tests between $primary_test_cluster and $secondary_test_cluster clusters"
+        export KUBECONFIG="$LOGS/$primary_test_cluster-kubeconfig.yaml:$LOGS/$secondary_test_cluster-kubeconfig.yaml"
+
+        INFO "Show all Submariner information"
+        subctl show all 2>&1 \
+            | tee  "$TESTS_LOGS/${tests_basename}_subctl_show_all.log" \
+            || add_test_error $?
+
+        INFO "Execute diagnose all tests"
+        subctl diagnose all --verbose \
+            --context "$primary_test_cluster" \
+            --context "$secondary_test_cluster" 2>&1 \
+            | tee "$TESTS_LOGS/${tests_basename}_subctl_diagnose_all.log" \
+            || add_test_error $?
+
+        INFO "Execute diagnose firewall inter-cluster tests"
+        subctl diagnose firewall inter-cluster --verbose \
+            --context "$primary_test_cluster" \
+            --remotecontext "$secondary_test_cluster" 2>&1 \
+            |  tee "$TESTS_LOGS/${tests_basename}_subctl_firewall_tests.log" \
+            || add_test_error $?
+
+        INFO "Execute E2E tests"
+        subctl verify --only service-discovery,connectivity --verbose \
+            --junit-report "$TESTS_LOGS/${tests_basename}_e2e_junit.xml" \
+            --context "$primary_test_cluster" \
+            --tocontext "$secondary_test_cluster" 2>&1 \
+            | tee "$TESTS_LOGS/${tests_basename}_subctl_e2e_tests.log" \
+            || add_test_error $?
+        unset KUBECONFIG
+    done
+    INFO "The E2E tests execution finished"
+}
--- lib/submariner_test/submariner_test.sh
+++ lib/submariner_test/submariner_test.sh
@@ -1,73 +1,47 @@
 #!/bin/bash
 
-# Perform Submariner test by using the "subctl" command.
+# Perform Submariner E2E tests by using the "subctl" command
+# and Submariner UI tests by using cypress tool.
 
-# The function executes Submariner E2E tests by using the subctl tool.
-# The subctl tool is able to run E2E tests only on two clusters
-# at the same time.
-# In order to be able to test all clusters which contains
-# submariner addon, the function below is using the first managed
-# cluster as a primary cluster and all other clusters will run E2E
-# tests with the primary cluster.
 function execute_submariner_tests() {
     INFO "Execute Submariner tests"
 
     rm -rf "$TESTS_LOGS"
     mkdir -p "$TESTS_LOGS"
 
-    # Subctl E2E tests are working with 2 clusters only at a time
-    local primary_test_cluster
-    local secondary_test_cluster
-    local tests_basename
+    verify_subctl_command
 
-    primary_test_cluster=$(echo "$MANAGED_CLUSTERS" | head -n 1)
+    if [[ "$TEST_TYPE" =~ "e2e" ]]; then
+        execute_submariner_e2e_tests
+    fi
 
-    for cluster in $MANAGED_CLUSTERS; do
-        if [[ "$cluster" == "$primary_test_cluster" ]]; then
-            continue
+    if [[ "$TEST_TYPE" =~ "ui" ]]; then
+        verify_cypress
+        # If cypress prerequisites are not fulfilled,
+        # cypress tests will be skipped.
+        if [[ "$UI_TESTS" == "false" ]]; then
+            WARNING "Skip UI tests execution - system is not ready"
+        else
+            execute_submariner_ui_tests
         fi
+    fi
 
-        secondary_test_cluster="$cluster"
-        tests_basename=$(combine_tests_basename "$primary_test_cluster" "$secondary_test_cluster")
-
-        INFO "Running tests between $primary_test_cluster and $secondary_test_cluster clusters"
-        export KUBECONFIG="$LOGS/$primary_test_cluster-kubeconfig.yaml:$LOGS/$secondary_test_cluster-kubeconfig.yaml"
-
-        INFO "Show all Submariner information"
-        subctl show all 2>&1 \
-            | tee  "$TESTS_LOGS/${tests_basename}_subctl_show_all.log" \
-            || add_test_error $?
-
-        INFO "Execute diagnose all tests"
-        subctl diagnose all --verbose \
-            --context "$primary_test_cluster" \
-            --context "$secondary_test_cluster" 2>&1 \
-            | tee "$TESTS_LOGS/${tests_basename}_subctl_diagnose_all.log" \
-            || add_test_error $?
-
-        INFO "Execute diagnose firewall inter-cluster tests"
-        subctl diagnose firewall inter-cluster --verbose \
-            --context "$primary_test_cluster" \
-            --remotecontext "$secondary_test_cluster" 2>&1 \
-            |  tee "$TESTS_LOGS/${tests_basename}_subctl_firewall_tests.log" \
-            || add_test_error $?
-
-        INFO "Execute E2E tests"
-        subctl verify --only service-discovery,connectivity --verbose \
-            --junit-report "$TESTS_LOGS/${tests_basename}_e2e_junit.xml" \
-            --context "$primary_test_cluster" \
-            --tocontext "$secondary_test_cluster" 2>&1 \
-            | tee "$TESTS_LOGS/${tests_basename}_subctl_e2e_tests.log" \
-            || add_test_error $?
-        unset KUBECONFIG
-    done
     INFO "Tests execution finished"
     INFO "All the logs are placed within the $TESTS_LOGS directory"
 }
 
+# The function will combine test report basename.
+# The function has types of tests it serves - "e2e" and "ui".
+# The e2e test basename will contain platforms in the basename:
+# - ACM-2.7.0-Submariner-0.14.1-AWS-GCP-Globalnet
+# It means need to provide all three args - "type", "primary_cluster", "second_cluster"
+# While UI test will skip platform details as it run only for the dashboard UI
+# - ACM-2.7.0-Submariner-0.14.1-Globalnet-UI
+# It means need to provide two args - "type" and "primary_cluster".
 function combine_tests_basename() {
-    local primary_cluster="$1"
-    local secondary_cluster="$2"
+    local type="$1"  # e2e or ui
+    local primary_cluster="$2"
+    local secondary_cluster="$3"
     local acm_ver
     local subm_ver
     local primary_cl_platform
@@ -80,8 +54,6 @@ function combine_tests_basename() {
         oc -n submariner-operator get subs submariner \
         -o jsonpath='{.status.currentCSV}' \
         | grep -Po '(?<=submariner.v)[^)]*' | cut -d '-' -f1)
-    primary_cl_platform=$(locate_cluster_platform "$primary_cluster")
-    secondary_cl_platform=$(locate_cluster_platform "$secondary_cluster")
 
     globalnet_state=$(KUBECONFIG="$LOGS/$primary_cluster-kubeconfig.yaml" \
         oc -n submariner-operator get pods -l=app=submariner-globalnet \
@@ -92,7 +64,14 @@ function combine_tests_basename() {
         globalnet="Globalnet"
     fi
 
-    echo "ACM-${acm_ver}-Submariner-${subm_ver}-${primary_cl_platform}-${secondary_cl_platform}-${globalnet}"
+    if [[ "$type" == "e2e" ]]; then
+        primary_cl_platform=$(locate_cluster_platform "$primary_cluster")
+        secondary_cl_platform=$(locate_cluster_platform "$secondary_cluster")
+
+        echo "ACM-${acm_ver}-Submariner-${subm_ver}-${primary_cl_platform}-${secondary_cl_platform}-${globalnet}"
+    elif [[ "$type" == "ui" ]]; then
+        echo "ACM-${acm_ver}-Submariner-${subm_ver}-${globalnet}-UI"
+    fi
 }
 
 # When one of the tests fails, add the error note to a global variable.
