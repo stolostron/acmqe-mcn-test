71dfd6d00bcf87b3f318fcdc026cc4320a0c38fc
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-01-17 15:20:15 +0000

    Add submariner install test to cypress (#175)
    -Add install test, does not include validation of the cluster number
    -Add validation of the submariner lables after deployment
    -create environment variable: NATT_PORT
    
    Co-authored-by: Noga Tal <ntal@redhat.com>

--- cypress/cypress/e2e/01_submariner_deployment_validation.cy.js
+++ cypress/cypress/e2e/01_submariner_deployment_validation.cy.js
@@ -24,27 +24,17 @@ describe('submariner - Deployment validation', {
         let clusterSetName = Cypress.env('CLUSTERSET_NAME')
         submarinerClusterSetMethods.submarinerClusterSetShouldExist(clusterSetName)
     })
-    
-    let testTheDataLabel = (dataLabel, textToHave, messageToHave) => {
-        cy.get(dataLabel).each(($el, index) => {
-            if (index > 0){
-                cy.wrap($el).click(40, 30).should('have.text', textToHave)
-                cy.get('.pf-c-popover__content').contains(messageToHave).should('exist').and('be.visible')
-            }
-        })
-        cy.get(dataLabel).eq(-1).click(40, 30)
-    }
 
     it('test the Connection status label', { tags: ['Connection'] }, function () {
-        testTheDataLabel('[data-label="Connection status"]', 'Healthy', 'established')
+        submarinerClusterSetMethods.testTheDataLabel('[data-label="Connection status"]', 'Healthy', 'established')
     })
 
     it('test the Agent status label', { tags: ['Agent'] }, function () {
-        testTheDataLabel('[data-label="Agent status"]', 'Healthy', 'is deployed on managed cluster')
+        submarinerClusterSetMethods.testTheDataLabel('[data-label="Agent status"]', 'Healthy', 'is deployed on managed cluster')
     })
 
     it('test the Gateway nodes labeled label', { tags: ['Gateway'] }, function () {
-        testTheDataLabel('[data-label="Gateway nodes labeled"]', 'Nodes labeled', 'submariner.io/gateway')
+        submarinerClusterSetMethods.testTheDataLabel('[data-label="Gateway nodes labeled"]', 'Nodes labeled', 'submariner.io/gateway')
     }) 
 })
 
