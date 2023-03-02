f34161c0ad0ac5eec591913ca47a93a99ef4d2e8
Noga Tal <ntal@redhat.com>
2023-03-02 10:59:38 +0200

    Adding verify of the Submariner deployment currently blocked and this work is on hold.

--- cypress/cypress/e2e/03_submariner_deploy.cy.js
+++ cypress/cypress/e2e/03_submariner_deploy.cy.js
@@ -8,6 +8,9 @@
 import { submarinerClusterSetMethods } from '../views/submariner/actions/submariner_actions'
 import { clusterSetMethods } from '../views/clusterset/clusterset'
 
+const cluster_aws = 'sub-1'
+const cluster_gcp =  'sub-2'
+
 describe('submariner - Deployment validation', {
     tags: ['@submariner'],
     retries: {
@@ -19,32 +22,35 @@ describe('submariner - Deployment validation', {
         cy.setAPIToken()
         cy.clearOCMCookies()
         cy.login()
+        cy.getClusterInfo(cluster_aws)
+        cy.getClusterInfo(cluster_gcp)
     })
 
     it('test the Submariner add-ons install', { tags: ['deployment'] }, function () {
-        let clusterSetName = Cypress.env('CLUSTERSET')
-        let nattPort = Cypress.env('SUBMARINER_IPSEC_NATT_PORT')
-        clusterSetMethods.createClusterSet(clusterSetName) 
-        submarinerClusterSetMethods.manageClusterSet(clusterSetName)
+        let clusterSetName = Cypress.env('CLUSTERSET')+'1'
+        // let nattPort = Cypress.env('SUBMARINER_IPSEC_NATT_PORT')
+        // clusterSetMethods.createClusterSet(clusterSetName) 
+        // submarinerClusterSetMethods.manageClusterSet(clusterSetName)
 
-        cy.get('button').contains('Save').click()
-        cy.contains('Submariner add-ons', {timeout: 3000}).should('exist').click()
-        cy.get('#install-submariner').click()
-        cy.get('#install-submariner').click()
-        cy.get('#available-clusters-input-toggle-select-multi-typeahead-typeahead').click()
-        cy.get('.pf-c-select__menu-item').click({multiple: true})
-        cy.get('#globalist-enable').click({ force: true })
-        cy.get('button').contains('Next').click()
-        cy.get('#natt-port').type('{selectAll}'+nattPort)
-        for (let i=0; i<2; i++) {
-            cy.get('button').contains('Next').click()
-        }
-        cy.get('button').contains('Install').click()
+        // cy.get('button').contains('Save').click()
+        // cy.contains('Submariner add-ons', {timeout: 3000}).should('exist').click()
+        // cy.get('#install-submariner').click()
+        // cy.get('#install-submariner').click()
+        // cy.get('#available-clusters-input-toggle-select-multi-typeahead-typeahead').click()
+        // cy.get('.pf-c-select__menu-item').click({multiple: true})
+        // cy.get('#globalist-enable').click({ force: true })
+        // cy.get('button').contains('Next').click()
+        // cy.get('#natt-port').type('{selectAll}'+nattPort)
+        // for (let i=0; i<2; i++) { //use variable
+        //     cy.get('button').contains('Next').click()
+        // }
+        // cy.get('button').contains('Install').click()
+    
+        // submarinerClusterSetMethods.testTheDataLabel('[data-label="Connection status"]', 'Healthy', 'established')
+        // submarinerClusterSetMethods.testTheDataLabel('[data-label="Agent status"]', 'Healthy', 'is deployed on managed cluster')
+        // submarinerClusterSetMethods.testTheDataLabel('[data-label="Gateway nodes labeled"]', 'Nodes labeled', 'submariner.io/gateway')
 
-        cy.timeout(210000)
-        submarinerClusterSetMethods.testTheDataLabel('[data-label="Connection status"]', 'Healthy', 'established')
-        submarinerClusterSetMethods.testTheDataLabel('[data-label="Agent status"]', 'Healthy', 'is deployed on managed cluster')
-        submarinerClusterSetMethods.testTheDataLabel('[data-label="Gateway nodes labeled"]', 'Nodes labeled', 'submariner.io/gateway')
+        submarinerClusterSetMethods.isDeploy(clusterSetName, cluster_aws, cluster_gcp)
     })
 })
 
--- cypress/cypress/views/submariner/actions/submariner_actions.js
+++ cypress/cypress/views/submariner/actions/submariner_actions.js
@@ -21,7 +21,7 @@ export const submarinerClusterSetMethods = {
     manageClusterSet: (clusterSetName) => {
         cy.get('[data-label="Name"]').contains(clusterSetName).click()
         cy.get('#clusters').contains('Go to Managed clusters').click()
-        cy.get('.pf-c-empty-state__primary > .pf-c-button').click()
+        cy.wait(100)
         cy.get('.pf-c-empty-state__primary > .pf-c-button').click()
         cy.get('[data-label="Name"]').each(($el, index) => {
             if (index > 0) {
@@ -47,7 +47,45 @@ export const submarinerClusterSetMethods = {
             }
         })
         cy.get(dataLabel).eq(-1).click(40, 30)
-    }
+    },
+
+    // The function checks if submariner was deployed properly
+    isDeploy: (clusterset_name, cluster_aws, cluster_gcp) => {
+        // Go to the submariner adds-on page
+        clusterSetMethods.clusterSetShouldExist(clusterset_name)
+        cy.get('[data-label=Name]').eq(1).click(15, 30)
+        cy.get('.pf-c-nav__link').contains('Submariner add-ons').click()
+                
+        // Wait until submariner installation on the aws cluster is completed 
+        cy.get('tr').contains(cluster_aws).then(($aws) => {
+            cy.wrap($aws).closest('tr').then(($tr) => {
+                cy.wrap($tr).waitUntilContains('[data-label="Connection status"]','Healthy', { timeout: 300000, interval: 3000 }) 
+            })
+        })
+        cy.get('tr').contains(cluster_gcp).then(($gcp) => {
+            cy.wrap($gcp).closest('tr').then(($tr) => {
+                cy.wrap($tr).waitUntilContains('[data-label="Connection status"]','Healthy', { timeout: 300000, interval: 3000 }) 
+            })
+        })
+
+        // Login to the GCP Managed Clutser (cluster_gcp) and execute the following commands on this cluster
+        cy.managedClusterLogin(Cypress.env(cluster_aws + '_api'), Cypress.env(cluster_aws + '_user'), Cypress.env(cluster_aws + '_pwd'))
+
+        cy.exec('oc -n default create deployment nginx --image=nginxinc/nginx-unprivileged:stable-alpine', {failOnNonZeroExit: false, timeout: 15000})
+        cy.exec('oc -n default expose deployment nginx --port=8080', {failOnNonZeroExit: false, timeout: 15000})
+        cy.exec('subctl export service --namespace default nginx', {failOnNonZeroExit: false, timeout: 15000}) // Should be replaced by ocp manifest
+        cy.exec("oc -n default run tmp-shell --rm -i --tty --image quay.io/submariner/nettest -- /bin/bash -c 'sleep 40 && curl nginx.default.svc.clusterset.local:8080'",
+         {failOnNonZeroExit: false, timeout: 15000}).then(output => {
+            //cy.wait(50000)
+            Object.entries(output).forEach((line) => {
+                cy.log(line)
+              })
+         })
+         
+            // .its('stdout')
+            // .should('contain','<html>')
+            // .should('contain','Welcome to nginx!')
+    },
 }
 
 
