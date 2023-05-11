b4c29358b146043b1ccd64e2143ed5e3b534de1c
Noga Tal <105503802+NogaTal98@users.noreply.github.com>
2023-05-11 12:53:30 +0300

    Update the cypress deployment test according to the fact that it is possible to select a catalog source (#322)
    - Using the catalog source selection on the to deploy submariner from UI.
    - Added an option to insert a list of cluster names in the manageClusterSet method.

--- cypress/cypress/views/submariner/actions/submariner_actions.js
+++ cypress/cypress/views/submariner/actions/submariner_actions.js
@@ -18,31 +18,40 @@ export const submarinerClusterSetMethods = {
 
     // The function adds the AWS an GCP clusters to the cluster set.
     // start position: from the 'Cluster set' list.
-    manageClusterSet: (clusterSetName) => {
+    manageClusterSet: (managed_clusters_list, clusterSetName) => {
+        cy.log(managed_clusters_list)
         cy.get('[data-label="Name"]').contains(clusterSetName).click()
         cy.get('#clusters').contains('Go to Managed clusters').click()
-        cy.get('.pf-c-empty-state__primary > .pf-c-button').click()
+        cy.wait(500)
         cy.get('.pf-c-empty-state__primary > .pf-c-button').click()
         cy.get('[data-label="Name"]').each(($el, index) => {
             if (index > 0) {
                 if (!$el.text().includes('local-cluster')){// skip the local
-                    cy.get('[data-label=Infrastructure]').eq(index).then(($platform) => { // get cluster patform
-                        if ($platform.text().includes('Google') || $platform.text().includes('Amazon')){
-                            cy.get('[type="checkbox"]').eq(index).click()
-                        }
-                    })
+                    if (managed_clusters_list.length > 0){
+                        cy.get('[data-label=Name]').eq(index).then(($name) => { // get clusters from given list
+                            if (managed_clusters_list.includes($name.text())){
+                                cy.get('[type="checkbox"]').eq(index).click()
+                            }
+                        })
+                    }
+                    else{
+                        cy.get('[data-label=Infrastructure]').eq(index).then(($platform) => { // get cluster patform
+                            if ($platform.text().includes('Google') || $platform.text().includes('Amazon')){
+                                cy.get('[type="checkbox"]').eq(index).click()
+                            }
+                        })
+                    }
                 }
             }
         })
         cy.get('#save').click()//review
     },
 
-
     // The function checks if the current data label exists and contains the correct message
     testTheDataLabel: (dataLabel, textToHave, messageToHave) => {
         cy.get(dataLabel).each(($el, index) => {
             if (index > 0){
-                cy.wrap($el).click(40, 30).should('have.text', textToHave)
+                cy.wrap($el).click(40, 30, { timeout: 300000, interval: 3000 }).should('have.text', textToHave, { timeout: 300000, interval: 3000 })
                 cy.get('.pf-c-popover__content').contains(messageToHave).should('exist').and('be.visible')
             }
         })
