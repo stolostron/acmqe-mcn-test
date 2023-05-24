0f5e64ceabcc403f032bba2a048fda100d135f8a
Maxim Babushkin <mbabushk@redhat.com>
2023-05-24 14:06:02 +0300

    Change "tests_logs" directory structure (#366)
    Until now, tests logs stored within the "tests_logs" directory.
    Both e2e and ui.
    When test execution started, "tests_logs" directory deleted in order to
    eliminate the risk of using tests results from previous tests
    executions.
    
    In jenkins pipeline, test execution has been separated into two separate
    stages - "Submariner Test - E2E" and "Submariner Test - Cypress UI".
    That lead to a state where second execution of tests (UI) deleted E2E
    tests results.
    
    Change the structure of "tests_logs" to the following:
    - Each test type will create and place its tests results in it's own
      directory under "tests_logs".
      Ex: "tests_logs/e2e" and "tests_logs/ui".
    - When each of the test types started, it will delete only it's own
      directory before executing the actual tests to eliminate the risk of
      mixing older tests results.
    
    That's way, even multiple, separated execution of tests will keep all
    logs at the end of the pipeline.

--- run.sh
+++ run.sh
@@ -93,6 +93,10 @@ function prepare() {
 }
 
 function deploy_submariner() {
+    # Before each deployment, make sure old tests logs removed
+    rm -rf "$TESTS_LOGS"
+    mkdir -p "$TESTS_LOGS"
+
     select_submariner_version_and_channel_to_deploy
 
     if [[ "$PLATFORM" =~ "rosa" ]]; then
