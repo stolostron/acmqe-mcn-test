500f7e8d47c76255533e39d32784f80ce735b637
Noga Tal <105503802+NogaTal98@users.noreply.github.com>
2023-08-01 14:19:31 +0300

    [release-2.6] Added creation of a log file for the UI tests (#482)
    At the end of running each UI test, a log file will be created

--- cypress/cypress/e2e/04_submariner_addon.cy.js
+++ cypress/cypress/e2e/04_submariner_addon.cy.js
@@ -22,10 +22,13 @@ describe('submariner - validate submariner addon tab', {
 
     it('test the Submariner add-ons', { tags: ['addon'] }, function () {
         let clusterSetName = Cypress.env('CLUSTERSET')+'-'+(Math.random() + 1).toString(36).substring(4)
+        cy.log('create a cluster set named ' + clusterSetName)
         clusterSetMethods.createClusterSet(clusterSetName)
         cy.get('.pf-c-text-input-group__text-input').type(clusterSetName)
         cy.contains(clusterSetName).should('exist').click({timeout: 3000})
         cy.get('.pf-c-nav__link').contains('Submariner add-ons').should('exist')
+        cy.log("submariner add-ons tab was found")
+        cy.log("delete the cluster set named " + clusterSetName)
         clusterSetMethods.deleteClusterSet(clusterSetName)
     })
 })
