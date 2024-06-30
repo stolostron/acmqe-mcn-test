47759b653d56e1b776fcbf15da731e71976d19eb
Maxim Babushkin <mbabushk@redhat.com>
2024-06-30 14:30:37 +0300

    Remove install steps for report polarion flow
    Since all the flow is running inside containers with all the
    requirements pre-installed, no separate install process for report is
    longer required.

--- lib/reporting/polarion.sh
+++ lib/reporting/polarion.sh
@@ -5,13 +5,6 @@
 
 function report_polarion() {
     INFO "Polarion: Report Polarion test state (internal only)"
-    local venv="/tmp/subm"
-
-    python3 -m venv "$venv"
-    # shellcheck source=/dev/null
-    source "$venv/bin/activate"
-    pip install -r requirements.txt
-
     INFO "Polarion: Process reports"
     python3 lib/reporting/polarion_report.py \
         --config "$POLARION_VARS_FILE" --path "$TESTS_LOGS"
